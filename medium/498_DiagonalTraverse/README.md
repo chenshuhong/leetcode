c36dfff6e82e6698da5e939652e6229e3ee7eb8e
shuhong <1573391005@qq.com>
2019-05-17 02:45:59 +0800

    leetcode498

--- medium/498_DiagonalTraverse/README.md
+++ medium/498_DiagonalTraverse/README.md
@@ -23,3 +23,44 @@ Explanation:
 
 The total number of elements of the given matrix will not exceed 10,000.
 
+遍历时有这个规则，斜向上的坐标和是偶数，斜向下的坐标和是奇数
+
+```js
+if (matrix.length === 0){
+    return []
+  }
+  let resultArray = []
+  let row = matrix.length
+  let col = matrix[0].length
+  let rowIndex = 0,colIndex = 0;
+  for (let i = 0;i<row*col;i++){
+    resultArray.push(matrix[rowIndex][colIndex])
+    if ((rowIndex+colIndex)%2 === 0){
+      if (colIndex === (col - 1)) {
+        // 往下移动一格准备向下遍历
+        rowIndex++;
+      } else if (rowIndex === 0){
+        // 往右移动一格准备向下遍历
+        colIndex++;
+      } else {
+        // 往上移动
+        rowIndex--;
+        colIndex++;
+      }
+    }else {
+      if (rowIndex === (row - 1)) {
+        // 往右移动一格准备向上遍历
+        colIndex++;
+      } else if (colIndex === 0){
+        // 往上移动一格准备向上遍历
+        rowIndex++;
+      } else {
+        // 往下移动
+        rowIndex++;
+        colIndex--;
+      }
+    }
+  }
+  return resultArray
+```
+
--- medium/498_DiagonalTraverse/index.js
+++ medium/498_DiagonalTraverse/index.js
@@ -9,30 +9,36 @@ var findDiagonalOrder = function(matrix) {
   let resultArray = []
   let row = matrix.length
   let col = matrix[0].length
-  let rowIndex=0,colIndex = 0
-  let direction = true //true表示斜向上，false表示斜向下
-  while ((rowIndex+colIndex)<=(row+col)){
-    if (rowIndex<row&&colIndex<col){
-      resultArray.push(matrix[rowIndex][colIndex])
-      console.log(rowIndex,colIndex)
-    }
-    if (rowIndex === 0 && direction ===true){
-      direction=false
-      colIndex++
-    } else if (colIndex === 0 && direction === false){
-      direction = true
-      rowIndex++
-    } else {
-      if (direction){
-        rowIndex--
-        colIndex++
-      }else {
-        rowIndex++
-        colIndex--
+  let rowIndex = 0,colIndex = 0;
+  for (let i = 0;i<row*col;i++){
+    resultArray.push(matrix[rowIndex][colIndex])
+    if ((rowIndex+colIndex)%2 === 0){
+      if (colIndex === (col - 1)) {
+        // 往下移动一格准备向下遍历
+        rowIndex++;
+      } else if (rowIndex === 0){
+        // 往右移动一格准备向下遍历
+        colIndex++;
+      } else {
+        // 往上移动
+        rowIndex--;
+        colIndex++;
+      }
+    }else {
+      if (rowIndex === (row - 1)) {
+        // 往右移动一格准备向上遍历
+        colIndex++;
+      } else if (colIndex === 0){
+        // 往上移动一格准备向上遍历
+        rowIndex++;
+      } else {
+        // 往下移动
+        rowIndex++;
+        colIndex--;
       }
     }
   }
   return resultArray
 };
 
-console.log(findDiagonalOrder([[1,2,3],[4,5,6],[7,8,9]]))
+console.log(findDiagonalOrder([[1,2,3],[4,5,6]]))
