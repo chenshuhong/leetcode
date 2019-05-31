ab7ce8ae51aa4ca9a5d6241526ffd6603adca8da
chenshuhong <1573391005@qq.com>
2019-05-31 19:06:37 +0800

    739每日温度

--- medium/739_DailyTemperatures/index.js
+++ medium/739_DailyTemperatures/index.js
@@ -3,5 +3,26 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(T) {
+  let resultArray = []
+  for (let i=0,length = T.length;i<length;i++){
+    for (let j=i;j<length;j++){
+      if (T[i]<T[j]){
+        resultArray[i] = j-i
+        break
+      }else if (j===length-1){
+        resultArray[i] = 0
+      }
+    }
+  }
+  return resultArray
+};
 
+/**
+ * @param {number[]} T
+ * @return {number[]}
+ */
+var dailyTemperatures2 = function(T) {
+  let resultArray = []
+  
+  return resultArray
 };
